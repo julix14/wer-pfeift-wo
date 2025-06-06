cda22848a84cbe94edaf76d4cd7c8f805e9a6713
Juliusbecker <julix.becker@gmail.com>
2025-06-06 19:40:57 +0200

    Add: Styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import { type Metadata } from 'next'
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
   SignedIn,
-  SignedOut,
   UserButton,
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Image from 'next/image'
 import Link from 'next/link'
 import './globals.css'
-import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -34,26 +32,24 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              
-                <Button>
-                <Link href="/sign-in">
-                  Anmelden
-                </Link>
-              </Button>
-            </SignedOut>
+          <header className="sticky top-0 z-50 bg-white flex justify-between items-center p-4 gap-4 h-16 border-b">
+              <Link href="/" className='flex items-center gap-2'>
+              <Image src="/sr-logo.svg" alt="SR Logo" width={28} height={28} />
+                <h1 className="text-2xl font-bold">Wer pfeift wo?</h1>
+              </Link>
+            <div className='flex items-center gap-4'>
             <SignedIn>
-              <Link 
-                href="/profile" 
+              <Link
+                href="/dfbnet-data" 
                 className="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
               >
-                Profile
+                DFBnet Daten
               </Link>
-              <UserButton />
+              <UserButton fallback={<Skeleton className="h-[28px] w-[28px] rounded-full" />}/>
             </SignedIn>
+            </div>
           </header>
           {children}
         </body>
